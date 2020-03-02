54bbf0082199bd4ed5fbb7b5b42f6273e5f030b1
Zhou <wz1119@ic.ac.uk>
2020-03-02 12:53:53 +0000

    Working MU0 CPU, Pipelining needs implementation

--- DECODER_CHECK.v
+++ DECODER_CHECK.v
@@ -17,7 +17,7 @@ module DECODER_CHECK
 	output add_sub,
 	output mux4
 );
-	wire LDA, STA, ADD, SUB, JMP, JMI, JEQ, STP, LDI, LSL, LSR, EQ, MI;
+	wire LDA, STA, ADD, SUB, JMP, JMI, JEQ, STP, LDI, LSL, LSR, EQ, MI, ASR;
 	assign LDA = ~OP[15]&~OP[14]&~OP[13]&~OP[12]; 	// LDA 0000 0
 	assign STA = ~OP[15]&~OP[14]&~OP[13]&OP[12];		// STA 0001 1
 	assign ADD = ~OP[15]&~OP[14]&OP[13]&~OP[12];		// ADD 0010 2
@@ -29,8 +29,9 @@ module DECODER_CHECK
 	
 	assign STP = ~OP[15]&OP[14]&OP[13]&OP[12];		// STP 0111 7
 	assign LDI = OP[15]&~OP[14]&~OP[13]&~OP[12];		// LDI 1000 8 
-	assign LSL = OP[15]&~OP[14]&~OP[13]&OP[12];		// LSL 1001 9
+	assign LSL = OP[15]&~OP[14]&~OP[13]&OP[12];		// LSL 1001 9 (Implement this)
 	assign LSR = OP[15]&~OP[14]&OP[13]&~OP[12];		// LSR 1010 A
+	assign ASR = OP[15]&~OP[14]&OP[13]&OP[12];      // ASR 1011 B 
 	assign EQ = ~ACC_OUT[15]&~ACC_OUT[14]&~ACC_OUT[13]&~ACC_OUT[12]&~ACC_OUT[11]&~ACC_OUT[10]&~ACC_OUT[9]&~ACC_OUT[8]&~ACC_OUT[7]&~ACC_OUT[6]&~ACC_OUT[5]&~ACC_OUT[4]&~ACC_OUT[3]&~ACC_OUT[2]&~ACC_OUT[1]&~ACC_OUT[0];
 	assign MI = ACC_OUT[15];
 
@@ -40,12 +41,12 @@ module DECODER_CHECK
 	assign MUX1 = EXEC1&(STA|ADD|SUB|LDA|(JMI&MI)|(JEQ&EQ)|JMP) | EXEC2&(ADD|SUB|LDA);
 	assign MUX3 = (LDA|LDI) | (EXEC1&(ADD|SUB)) ;
 	assign SLOAD = (JMP | (JEQ&EQ) | (JMI&MI))&EXEC1; 
-	assign CNT_EN = EXEC2&(LDA|ADD|SUB) | EXEC1&(LDI|STA|LSL|LSR|(JMI&~MI)|(JEQ&~EQ));
+	assign CNT_EN = EXEC2&(LDA|ADD|SUB) | EXEC1&(ASR|LDI|STA|LSL|LSR|(JMI&~MI)|(JEQ&~EQ));
 	assign WREN = STA&EXEC1;
 	assign SLOAD_ACC = (LDI)&EXEC1 | (SUB|ADD|LDA)&EXEC2;
 	assign add_sub = ADD;
-	assign shift = 0;
-	assign enable_acc = (LDI|LSL|LSR)&EXEC1 | (SUB|ADD|LDA)&EXEC2;
+	assign shift = ASR&EXEC1;
+	assign enable_acc = (LDI|LSL|LSR|ASR)&EXEC1 | (SUB|ADD|LDA)&EXEC2;
 	assign mux4 = EXEC1&LSR;
 	
 endmodule
